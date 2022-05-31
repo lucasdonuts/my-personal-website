56ce4283534a57d94e3b335f5d5779c4b9d69462
Lucas Duncan <lucasdonuts@gmail.com>
2022-05-31 00:43:40 -0500

    Added interactive buttons

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This text added by Javascript";
+document.getElementById("first-button").addEventListener('click', addSecondButtonSet);
 
-document.getElementById("content").appendChild(h2);
+function addSecondButtonSet() {
+  const element = document.createElement("p");
+  element.class = "button-row";
+
+  const buttonOne = document.createElement("button");
+  buttonOne.className = "second-button-set";
+  buttonOne.id = "red-on-click";
+  buttonOne.innerText = "I turn red";
+  buttonOne.style.marginRight = "10px";
+  element.append(buttonOne);
+
+  buttonOne.addEventListener('click', function() {
+    buttonOne.style.backgroundColor = "red";
+  })
+
+  const buttonTwo = document.createElement("button");
+  buttonTwo.className = "second-button-set";
+  buttonTwo.id = "yellow-on-click";
+  buttonTwo.innerText = "I turn yellow";
+  element.append(buttonTwo);
+
+  buttonTwo.addEventListener('click', function() {
+    buttonTwo.style.backgroundColor = "yellow";
+  })
+
+  const buttonsDiv = document.getElementById("buttons-div");
+  buttonsDiv.append(element);
+}
+
+{/* <p class="button-row">
+          <button type="button" class="second-button-set">Button 1</button>
+          <button type="button" class="second-button-set">Button 2</button>
+        </p> */}
